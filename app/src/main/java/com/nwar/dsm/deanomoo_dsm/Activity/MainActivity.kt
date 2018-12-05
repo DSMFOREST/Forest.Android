8f711fc647c20a8fab11643cca5814397ff2fd1a
nvvar <nwar0825@users.noreply.github.com>
2018-12-05 12:18:28 +0900

    게시물 리스트 구현.

--- app/src/main/java/com/nwar/dsm/deanomoo_dsm/Activity/MainActivity.kt
+++ app/src/main/java/com/nwar/dsm/deanomoo_dsm/Activity/MainActivity.kt
@@ -2,12 +2,36 @@ package com.nwar.dsm.deanomoo_dsm.Activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.nwar.dsm.deanomoo_dsm.Adapter.PosterAdapter
+import com.nwar.dsm.deanomoo_dsm.DataModule.Poster
 import com.nwar.dsm.deanomoo_dsm.R
 
 class MainActivity : AppCompatActivity() {
 
+    var posterList = arrayListOf<Poster>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setPosterList()
+        setRecyclerView()
+    }
+
+    fun setPosterList(){
+        for (i in 1..11){
+            posterList.add(Poster("$i"+"번째 대마", "대마숲 $i"+"번째", null))
+        }
+    }
+
+    fun setRecyclerView(){
+        val adapter = PosterAdapter(this, posterList)
+        val lm = LinearLayoutManager(this)
+        val recyclerView = findViewById<RecyclerView>(R.id.list_poster_list)
+
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = lm
+        recyclerView.setHasFixedSize(true)
     }
 }
---
+++ app/src/main/java/com/nwar/dsm/deanomoo_dsm/Adapter/PosterAdapter.kt
@@ -0,0 +1,39 @@
+package com.nwar.dsm.deanomoo_dsm.Adapter
+
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.nwar.dsm.deanomoo_dsm.DataModule.Poster
+import com.nwar.dsm.deanomoo_dsm.R
+
+
+class PosterAdapter (val context: Context, val items : ArrayList<Poster>):RecyclerView.Adapter<PosterAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
+        val view : ViewHolder = ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_item,p0,false))
+        return view
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(p0: PosterAdapter.ViewHolder, p1: Int) {
+        p0.bind(items[p1],context)
+    }
+
+    inner class ViewHolder(view : View) : RecyclerView.ViewHolder(view){
+        val title = view?.findViewById<TextView>(R.id.title_poster_tv)
+        val content = view?.findViewById<TextView>(R.id.content_poster_tv)
+        val picture = view?.findViewById<ImageView>(R.id.image_poster_iv)
+        fun bind(posterInfo : Poster, context: Context){
+            title?.text = posterInfo.title
+            content?.text = posterInfo.content
+        }
+    }
+}
